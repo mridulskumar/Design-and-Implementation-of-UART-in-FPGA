09b7faffc94d6493fcdced39806be7062e5c4d08
Mridul S Kumar <149874226+mridulskumar@users.noreply.github.com>
2025-11-02 11:52:11 +0530

    uart_top.v

--- uart_top.v
+++ uart_top.v
@@ -1,25 +1,4 @@
 `timescale 1ns / 1ps
-//////////////////////////////////////////////////////////////////////////////////
-// Company: 
-// Engineer: 
-// 
-// Create Date: 17.10.2025 18:05:07
-// Design Name: 
-// Module Name: uart_top
-// Project Name: 
-// Target Devices: 
-// Tool Versions: 
-// Description: 
-// 
-// Dependencies: 
-// 
-// Revision:
-// Revision 0.01 - File Created
-// Additional Comments:
-// 
-//////////////////////////////////////////////////////////////////////////////////
-
-
 module uart_top #(
     parameter CLK_FREQ = 50_000_000,
     parameter BAUD_RATE = 19200
